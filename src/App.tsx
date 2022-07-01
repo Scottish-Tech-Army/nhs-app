ccbb24e9edb496d6f4d60d5458ce5663e956d462
Byron Lee <byronn.lee@gmail.com>
2022-07-02 00:13:43 +0100

    Started comments through app dev flow

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import StorageArea from './StorageArea';
 
 import Box from './Box';
 
+
+//React-router V6+ method of wrapping routing app
+  //home route defined as Storage Area
+  //Nested routes use dynamic variables boxTemplate and boxId from Box component props lifted through setBoxContents function
+    //Nested route values passed into setBoxContents function
 function App() {
   return (
     <Router>
--- src/Box.tsx
+++ src/Box.tsx
@@ -12,26 +12,41 @@ export interface BoxProps {
 }
 
 function Box({ setBoxContents }: BoxProps) {
-  let { boxTemplateId, boxId } = useParams();
 
+//init variables
+  //destructure nested routes from useParams  
+  let { boxTemplateId, boxId } = useParams();
+  //init boxTemplate var finding boxTemplateId from TRAUMA_TOWER_TEMPLATE Doc
   const boxTemplate = TRAUMA_TOWER_TEMPLATE.boxes.find(
     (box) => box.boxTemplateId === boxTemplateId
   );
+  //init var of mapped items in template creating obj of name and count/amount of each
   const startingArrayOfObjects: FormValueType[] =
-    boxTemplate?.items.map(({ name }) => ({ name, count: 0 })) || [];
+      boxTemplate?.items.map(({ name }) => ({ name, count: 0 })) || [];
+  //init- useState hook using mapped items stored as init value  
   const [formValues, setFormValues] = useState<FormValueType[]>(
     startingArrayOfObjects
   );
-
-  let boxNumber = 0;
+  //init boxNumber to zero   
+    let boxNumber = 0;
+  //check boxId matches string of starting digit/end digit  
+    //parse boxId as Int
+    //it should so continue(maybe integrate fail condition?)
   if (boxId?.match(/^\d+$/)) {
     boxNumber = Number.parseInt(boxId);
   }
-
+  //check- no boxTemplate OR boxNumber less than 1 OR boxNumber is more than boxTemplate count/value
+    //if true return null (probably should return Error)
+    //if false continue
   if (!boxTemplate || boxNumber < 1 || boxNumber > boxTemplate.count) {
     return null;
   }
 
+  //Functions for storing form values on user input- form change/submit
+    //handle change- capture change to useState, set change to setFormValues
+        //init- form new form values to array
+        //process values to Int
+        //set to form values state
   let handleChange = (index: number, target: HTMLInputElement) => {
     let newFormValues = [...formValues];
 
@@ -40,6 +55,9 @@ function Box({ setBoxContents }: BoxProps) {
     setFormValues(newFormValues);
   };
 
+   //handleSubmit- pass router values and inputted state values  
+        //handle browser event to keep inputted data
+        //pass router values and inputted form values to setBoxContents function
   let handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setBoxContents(boxTemplateId!, boxNumber, formValues);
