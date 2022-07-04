1e341bbbbc92f4c1283dc2a30522829484dd4db2
Donal Stewart <covidresponse@demonsoft.org>
2022-07-04 12:47:01 +0100

    Added disclaimer popup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,19 +3,19 @@
 import { getByRole, screen } from "@testing-library/react";
 import React from "react";
 import App from "./App";
-import userEvent from "@testing-library/user-event";
 import { renderWithProvider } from "./testUtils";
 
 describe("App", () => {
-  it("renders default view", () => {
-    renderWithProvider(<App />);
+  it("renders default view", async () => {
+    const { user } = renderWithProvider(<App />);
+    await user.click(screen.getByRole("button", { name: "Accept" }));
+
     expect(screen.getByText("Trauma Tower")).toBeDefined();
   });
 
   it("renders shopping list view", async () => {
-    const user = userEvent.setup();
-
-    renderWithProvider(<App />);
+    const { user } = renderWithProvider(<App />);
+    await user.click(screen.getByRole("button", { name: "Accept" }));
 
     await user.click(screen.getByRole("link", { name: "Items needed" }));
 
@@ -23,9 +23,8 @@ describe("App", () => {
   });
 
   it("renders box view", async () => {
-    const user = userEvent.setup();
-
-    renderWithProvider(<App />);
+    const { user } = renderWithProvider(<App />);
+    await user.click(screen.getByRole("button", { name: "Accept" }));
 
     await user.click(
       screen.getByText("Trauma Chest Drain - Box 2").previousElementSibling!
@@ -39,9 +38,8 @@ describe("App", () => {
   });
 
   it("renders item view from box view", async () => {
-    const user = userEvent.setup();
-
-    renderWithProvider(<App />);
+    const { user } = renderWithProvider(<App />);
+    await user.click(screen.getByRole("button", { name: "Accept" }));
 
     await user.click(
       screen.getByText("Trauma Chest Drain - Box 2").previousElementSibling!
@@ -58,5 +56,23 @@ describe("App", () => {
 
     expect(screen.getByText("Item Details")).toBeDefined();
     expect(screen.getByText("Chest drain catheter")).toBeDefined();
+  });
+
+  it("disclaimer must be accepted to continue", async () => {
+    const { user } = renderWithProvider(<App />);
+
+    expect(
+      screen.getByText(
+        "This application is for demo use only. It is not intended for real life use."
+      )
+    ).toBeDefined();
+
+    await user.click(screen.getByRole("button", { name: "Accept" }));
+
+    expect(
+      screen.queryByText(
+        "This application is for demo use only. It is not intended for real life use."
+      )
+    ).toBeNull();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import StorageArea from "./StorageArea";
@@ -8,6 +8,8 @@ import ShoppingList from "./ShoppingList";
 import ItemDetails from "./ItemDetails";
 
 function App() {
+  const [disclaimerAccepted, setDisclaimerAccepted] = useState(false);
+
   return (
     <div className="root">
       <Routes>
@@ -16,6 +18,25 @@ function App() {
         <Route path="needed" element={<ShoppingList />} />
         <Route path="*" element={<StorageArea />} />
       </Routes>
+      {!disclaimerAccepted && (
+        <>
+          <div className="disclaimer-popup">
+            <h2>Disclaimer</h2>
+            <div className="message">
+              This application is for demo use only. It is not intended for real
+              life use.
+            </div>
+            <button
+              type="button"
+              className="confirm-yes"
+              onClick={() => setDisclaimerAccepted(true)}
+            >
+              Accept
+            </button>
+          </div>
+          <div className="modal-overlay" />
+        </>
+      )}
     </div>
   );
 }
